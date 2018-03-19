5bb0da5f6bb03090f81dde61b74d4972c1d30935
Ewan <jojothegreat694@hotmail.com>
2018-03-19 19:43:35 +0000

    Update fetchWeatherData.js

--- modules/fetchWeatherData.js
+++ modules/fetchWeatherData.js
@@ -9,7 +9,7 @@ event.on("fetchWeatherData", () => {
 	
 	//If user hasn"t set user info they can still play with this function
 
-	   	request("http://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=b1b15e88fa797225412429c1c50c122a1", (error, response,body) => {
+	   	request("insert", (error, response,body) => {
 
 	        if (error) {event.emit("error", new Error("fetchweatherData: " + error))}
 
